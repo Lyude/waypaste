042447b159cda7a997f2aa49c58223f097fef7cc
Lyude <thatslyude@gmail.com>
2016-11-27 20:12:56 -0500

    Initial

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name="waypaste",
+    version="0.1",
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'waypaste = waypaste.main:main'
+        ]
+    },
+
+    install_requires=['pywayland'],
+
+    author="Lyude Paul",
+    author_email="thatslyude@gmail.com",
+    description="CLI paste utility for wayland desktop environments"
+)
